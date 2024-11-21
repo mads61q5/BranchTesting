1f149754e888455b453a6113ee9dd9349a1dfb04
mads61q5 <mads61q5@stud.kea.dk>
2024-11-21 14:36:35 +0100

    Implemented eat

--- src/Dog.java
+++ src/Dog.java
@@ -3,4 +3,9 @@ public class Dog extends Animal{
     public void makeSound() {
         System.out.println("Woof Woof!");
     }
+
+    @Override
+    public boolean eat(String foodType) {
+        return false;
+    }
 }
